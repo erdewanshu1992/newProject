014c4a36e9323eeb242997a37fcc2c715abc3fce
dewanshu <qadev432@gmail.com>
2022-12-22 18:36:27 +0530

    FileUtils dependency

--- src/main/java/com/BrowserSetup/GoogleTestNg.java
+++ src/main/java/com/BrowserSetup/GoogleTestNg.java
@@ -1,15 +1,22 @@
 package com.BrowserSetup;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class GoogleTestNg {
 	@Test
-	public void browserLaunch() {
+	public void browserLaunch() throws IOException {
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.co.in/");
 		driver.get("https://www.yesmadam.com/");
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrFile, new File("screenshot.png"));
 	}
 
 }
